6eadab2449b98499a52f4fdc9a91a59ba219b3b7
Blad3Mak3r <blademaker.live@gmail.com>
2022-12-04 23:29:23 +0100

    Add errorType to DeezerException and wrap efficiently.

--- main/src/main/java/com/github/topisenpai/lavasrc/deezer/DeezerException.java
+++ main/src/main/java/com/github/topisenpai/lavasrc/deezer/DeezerException.java
@@ -7,17 +7,29 @@ public class DeezerException extends IllegalStateException {
     final Long errorCode;
     final String errorMessage;
 
-    private DeezerException(String message, Long errorCode, String errorMessage) {
-        super(message + ": (" + errorCode + ") " + errorMessage);
+    final String errorType;
+
+    private DeezerException(String message, Long errorCode, String errorMessage, String errorType) {
+        super(message + ": (" + errorCode + ": " + errorType + ") " + errorMessage);
 
         this.errorCode = errorCode;
         this.errorMessage = errorMessage;
+        this.errorType = errorType;
     }
 
-    static DeezerException wrap(String message, JsonBrowser json) {
+    static DeezerException wrapFromPrivateAPI(String message, JsonBrowser json) {
         var errorCode = json.get("data").index(0).get("errors").index(0).get("code").asLong(0);
         var errorMessage = json.get("data").index(0).get("errors").index(0).get("message").safeText();
+        var errorType = json.get("data").index(0).get("errors").index(0).get("type").safeText();
 
-        return new DeezerException(message, errorCode, errorMessage);
+        return new DeezerException(message, errorCode, errorMessage, errorType);
+    }
+
+    static DeezerException wrapFromPublicAPI(String message, JsonBrowser json) {
+        var errorCode = json.get("error").get("code").asLong(0);
+        var errorMessage = json.get("error").get("message").safeText();
+        var errorType = json.get("errors").get("type").safeText();
+
+        return new DeezerException(message, errorCode, errorMessage, errorType);
     }
 }
